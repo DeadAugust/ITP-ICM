bce672e7ba3fca3d467f712be54b11ef9d3049e5
DeadAugust <augustluhrs@gmail.com>
2018-11-29 03:11:12 -0500

    heroku test

--- Skinflint/demo_001/server.js
+++ Skinflint/demo_001/server.js
@@ -13,7 +13,9 @@ function Atman(id, x, y, fud, r, g, b){
 
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+// var server = app.listen(3000);
+
+var server = app.listen(process.env.PORT || 3000, listen);
 
 function listen() {
   var host = server.address().address;
