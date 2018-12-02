488b4d728e7cdb5548571dbe635933f1dbcc7c6d
DeadAugust <augustluhrs@gmail.com>
2018-12-02 14:20:37 -0500

    new prototype repo

---
+++ Skinflint/playable_002/public/sketch.js
@@ -0,0 +1,100 @@
+var socket;
+
+var peep;
+var peeps = [];
+
+function setup() {
+	createCanvas(400, 400);
+	background(0,100,100);
+	socket = io.connect('http://localhost:3000');
+//set up with socket.id?
+	peep = new Peep (socket.id, random(50, width - 50), random(50, height-50));
+
+	var data = {
+		id: peep.id,
+    x: peep.x,
+    y: peep.y
+  };
+
+	socket.emit('start', data);
+
+	socket.on('heartbeat',
+		function(data){
+			//console.log(data);
+			peeps = data;
+		}
+	);
+}
+
+function draw() {
+	for (var i = peeps.length - 1; i >= 0; i--){
+		var id = peeps[i].id;
+		//console.log(id);
+		if (id !== socket.id){
+			fill(255);
+			ellipse(peeps[i].x, peeps[i].y, 30, 30);
+
+			fill(0);
+			textAlign(CENTER);
+			textSize(18);
+			text(peeps[i].id, peeps[i].x, peeps[i].y);
+		}
+	}
+	peep.show(); //LOL
+	textSize(18);
+	fill(0);
+	text('me', peep.x - 2, peep.y + 5);
+
+	var data = {
+		// id? not needed
+		x: peep.x,
+		y: peep.y
+		// s?
+	};
+	socket.emit('update', data);
+	socket.on('msg',
+		function (data){
+			console.log('msg from: ' + data.idFrom);
+		}
+	);
+}
+
+function mousePressed(){
+	/*
+	if((mouseX >= peep.x - 40) && (mouseX <= peep.x + 40)
+    && (mouseY >= peep.y - 40) && (mouseY <= peep.y + 40)){
+      clearTrade();
+  }
+	*/
+	for (var i = peeps.length - 1; i >=0; i--){ //click to send msg
+		if((mouseX >= peeps[i].x - 30) && (mouseX <= peeps[i].x + 30)
+			&& (mouseY >= peeps[i].y - 30) && (mouseY <= peeps[i].y + 30)){
+				//clearTrade();
+				//peeps[i].s = true;
+				fill(255,255,0);
+				ellipse(peeps[i].x, peeps[i].y, 40, 40);
+				//target = true;
+				console.log(peeps[i].id);
+				var data = {
+					idTo: peeps[i].id,
+					idFrom: socket.id
+				}
+				socket.emit('msg', data);
+		}
+	}
+}
+
+function Peep(id, x, y){
+  this.id = id;
+	this.x = x;
+  this.y = y;
+  // this.s = false; //if selected
+
+  this.show = function(){
+    fill(0, 20, 255);
+    ellipse(this.x, this.y, 40, 40);
+  }
+
+  //this.inventory
+  //this.trade
+}
---
+++ Skinflint/playable_002/server.js
@@ -0,0 +1,64 @@
+var peeps = [];
+
+function Peep(id, x, y){
+  this.id = id;
+  this.x = x;
+  this.y = y;
+}
+
+
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log('Socket server running');
+
+var io = require('socket.io')(server);
+
+//heartbeat needed?
+setInterval(heartbeat, 33);
+function heartbeat(){ //so this is the only thing sent from server???
+  io.sockets.emit('heartbeat', peeps);
+}
+
+io.sockets.on('connection',
+  function(socket){
+    console.log("new peep: " + socket.id);
+    socket.on('start',
+      function(data){
+        var peep = new Peep(socket.id, data.x, data.y);
+        peeps.push(peep);
+      }
+    );
+
+    socket.on('update', //x undefined error from being first to party?
+      function(data){
+        //console.log(peeps.length);
+        if (peeps.length >= 2){ //so only starts if at least 2 players?
+          var peep;
+          for (var i = 0; i < peeps.length; i++){
+            if (socket.id == peeps[i].id){
+              peep = peeps[i];
+            }
+          }
+          // peep.x = data.x;
+          // peep.y = data.y
+        }
+      }
+    );
+
+    socket.on('msg',
+      function(data){
+        socket.broadcast.to(data.idTo).emit('msg', data);
+        console.log(data);
+      }
+    );
+
+    socket.on('disconnect',
+      function(data){
+        console.log("Client has disconnected");
+      })
+  })
